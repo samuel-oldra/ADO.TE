4762ed5e35e580f89839a47a6d32d1a0a108a9b0
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-10-09 00:00:00 -0300

    Editado arquivo README.md

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-## Workshop Semana React e Python da Treinaweb (Front-end)
-
-Repositório original:
-
-https://github.com/treinaweb/workshop-myteacher-react
+# Temp. Repo. 002
--- src/services/ApiService.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const ApiService = axios.create({
-    baseURL: 'http://127.0.0.1:8000',
-    headers: {
-        'Content-Type': 'application/json'
-    }
-});
