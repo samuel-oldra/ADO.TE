51637c68f828ef2c67002d48d442d57373c24e25
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-10-23 00:00:00 -0300

    Validações e mensagens de erro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,6 @@
+from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.messages import constants
 from django.shortcuts import render
 
 
@@ -13,9 +15,11 @@ def cadastro(request):
 
         if len(nome.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0 or len(
                 confirmar_senha.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
             return render(request, 'cadastro.html')
 
         if senha != confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'Digite duas senhas iguais')
             return render(request, 'cadastro.html')
 
         try:
@@ -24,6 +28,9 @@ def cadastro(request):
                 email=email,
                 password=senha,
             )
+
+            messages.add_message(request, constants.ERROR, 'Usuário criado com sucesso')
             return render(request, 'cadastro.html')
         except:
+            messages.add_message(request, constants.ERROR, 'Erro interno do sistema')
             return render(request, 'cadastro.html')
