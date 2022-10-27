2e01aa4f660ad71bd34e8c810d75172bc3212cf0
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-10-27 00:00:00 -0300

    Adicionados models para tag, raça e pet

--- adote/urls.py
+++ adote/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('usuarios.urls')),
+    # path('divulgar/', include('divulgar.urls')),
 ]
