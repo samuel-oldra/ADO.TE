bd651cdcf317ac909555887687b097e7f29d9217
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-11-03 00:00:00 -0300

    Cadastra um pet

--- adote/urls.py
+++ adote/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -20,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('usuarios.urls')),
     path('divulgar/', include('divulgar.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- divulgar/views.py
+++ divulgar/views.py
@@ -1,7 +1,9 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.messages import constants
 from django.shortcuts import render
 
-from .models import Tag, Raca
+from .models import Tag, Raca, Pet
 
 
 @login_required
@@ -9,4 +11,38 @@ def novo_pet(request):
     if request.method == "GET":
         tags = Tag.objects.all()
         racas = Raca.objects.all()
+        return render(request, 'novo_pet.html', {'tags': tags, 'racas': racas})
+    elif request.method == "POST":
+        foto = request.FILES.get('foto')
+        nome = request.POST.get('nome')
+        descricao = request.POST.get('descricao')
+        estado = request.POST.get('estado')
+        cidade = request.POST.get('cidade')
+        telefone = request.POST.get('telefone')
+        tags = request.POST.getlist('tags')
+        raca = request.POST.get('raca')
+
+        # TODO: Validar dados
+
+        # Primeiro salva os dados do Pet
+        pet = Pet(
+            usuario=request.user,
+            foto=foto, nome=nome,
+            descricao=descricao,
+            estado=estado,
+            cidade=cidade,
+            telefone=telefone,
+            raca_id=raca,
+        )
+        pet.save()
+
+        # Depois salva as tags relacionadas ao Pet
+        for tag_id in tags:
+            tag = Tag.objects.get(id=tag_id)
+            pet.tags.add(tag)
+        pet.save()
+
+        tags = Tag.objects.all()
+        racas = Raca.objects.all()
+        messages.add_message(request, constants.SUCCESS, 'Novo pet cadastrado')
         return render(request, 'novo_pet.html', {'tags': tags, 'racas': racas})
