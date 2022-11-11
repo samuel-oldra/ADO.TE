d0e8787f2637a079a428673b3586491930dbf7ae
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-11-11 00:00:00 -0300

    Adicionada model para pedido de adoção

--- adotar/models.py
+++ adotar/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from divulgar.models import Pet
+
+
+class PedidoAdocao(models.Model):
+    choices_status = (
+        ('AG', 'Aguardando aprovação'),
+        ('AP', 'Aprovado'),
+        ('R', 'Recusado')
+    )
+
+    pet = models.ForeignKey(Pet, on_delete=models.DO_NOTHING)
+    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    data = models.DateTimeField()
+    status = models.CharField(max_length=2, choices=choices_status, default='AG')
