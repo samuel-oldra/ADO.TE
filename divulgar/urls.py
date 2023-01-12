375e86bdee6c06bbe878f90d24d88646f29f2502
Robson Ribeiro <robsont36@gmail.com>
2023-01-11 23:54:25 -0300

    feat: adicionando nova url dashboard

--- divulgar/urls.py
+++ divulgar/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('dashboard/', views.all_pets, name="dashboard"),
     path('novo_pet/', views.novo_pet, name="novo_pet"),
 ]
