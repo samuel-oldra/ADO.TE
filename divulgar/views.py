3287b318818a12604612919bc8c8c5363b37bd05
Robson Ribeiro <robsont36@gmail.com>
2023-01-11 23:54:41 -0300

    feat: adicionando novo metodo para listar os pets

--- divulgar/views.py
+++ divulgar/views.py
@@ -1,12 +1,17 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from .models import Tag, Raca
+from .models import Pet, Raca, Tag
 
 
 @login_required
-def novo_pet(request):
+def all_pets(request):
     if request.method == "GET":
-        tags = Tag.objects.all()
-        racas = Raca.objects.all()
-        return render(request, 'novo_pet.html', {'tags': tags, 'racas': racas})
+        pets = Pet.objects.all()
+        return render(request, 'dashboard.html', {'pets': pets})
+
+@login_required
+def novo_pet(request):
+    tags = Tag.objects.all()
+    racas = Raca.objects.all()
+    return render(request, 'novo_pet.html', {'tags': tags, 'racas': racas})
